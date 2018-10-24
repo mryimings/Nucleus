1de3aa623c3c837492c837c501d9500179155d8c
ysun647 <yimingsun1994@gmail.com>
2018-10-23 20:12:00 -0400

    urlfor href to another

--- application.py
+++ application.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/another')
+def another():
+    return render_template('another.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
