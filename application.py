78f24cce4aae1fec127584f5a81487c790c89c74
sym19940907 <sym19940907@users.noreply.github.com>
2018-10-23 20:19:05 -0400

    added temp signup and login page

--- application.py
+++ application.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/another')
-def another():
-    return render_template('another.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.debug = True
