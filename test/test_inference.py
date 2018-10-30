7586eae875824e04fb104c1730b20c10c0b0c47a
Minghao Li <minghao95@outlook.com>
2018-10-29 22:55:14 -0400

    test 1 of hooks pre-commit

--- test/test_inference.py
+++ test/test_inference.py
@@ -1,10 +1,9 @@
 import unittest
-import sys
 import os
 from os.path import dirname, abspath
-#sys.path.insert(0, '../models/r_net')
-#from models.r_net import inference
 parent_dir = dirname(dirname(abspath(__file__)))
+
+# 123
 
 # the test cases for database
 class database_test_cases(unittest.TestCase):
