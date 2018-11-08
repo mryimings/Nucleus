d42add91c298ed84acefb68fd9ef6d53e04324ea
ysun647 <yimingsun1994@gmail.com>
2018-11-08 14:31:40 -0500

    added rake-nltk and wikipedia, asking without context wait to be test

--- cognito.py
+++ cognito.py
@@ -1,15 +1,16 @@
 from warrant import Cognito
 from config import cognito_userpool_id, cognito_app_client_id
+import wikipedia
 
-u = Cognito(cognito_userpool_id, cognito_app_client_id)
-# u.delete_user()
-
-u.add_base_attributes(email='yimingsun1994@gmail.com')
+# u = Cognito(cognito_userpool_id, cognito_app_client_id)
+# # u.delete_user()
 #
-u.register('yiming', 'abcd1234')
-
-# x = u.confirm_sign_up('351695', username='minghao')
-# print(x)
-
-# au = u.authenticate('1234')
-# print(au, type(au))
+# u.add_base_attributes(email='yimingsun1994@gmail.com')
+# #
+# u.register('yiming', 'abcd1234')
+#
+# # x = u.confirm_sign_up('351695', username='minghao')
+# # print(x)
+#
+# # au = u.authenticate('1234')
+# # print(au, type(au))
