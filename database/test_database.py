553e8efbdaa626a1e8e2428091143952410007f1
Minghao Li <minghao95@outlook.com>
2018-11-14 13:56:40 -0500

    change test database import

--- database/test_database.py
+++ database/test_database.py
@@ -1,4 +1,9 @@
 import unittest
+
+import sys
+from os.path import dirname, abspath
+d = dirname(dirname(abspath(__file__)))
+sys.path.append(d)
 from database.db_update_class import db
 
 def count_rows(cursor):
