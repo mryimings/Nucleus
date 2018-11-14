c2bf01461de83188f60cf63dceb048ba9cae0a20
Minghao Li <minghao95@outlook.com>
2018-11-14 14:15:44 -0500

    format codes

--- test/test_inference.py
+++ test/test_inference.py
@@ -1,7 +1,9 @@
-import unittest
 import os
+import unittest
 from os.path import dirname, abspath
+
 parent_dir = dirname(dirname(abspath(__file__)))
+
 
 # the test cases for database
 class database_test_cases(unittest.TestCase):
@@ -11,6 +13,7 @@ class database_test_cases(unittest.TestCase):
     def test_infer(self):
         res = os.popen(self.com_default).read()
         print(res)
+
 
 if __name__ == '__main__':
     unittest.main()
