3612b8eaab829098d7a16cabc18e06f4e7d5fcba
ysun647 <yimingsun1994@gmail.com>
2018-12-12 14:47:01 -0500

    updated feedback, now functional

--- application.py
+++ application.py
@@ -179,7 +179,6 @@ def feedback(question=None, answer=None):
     if 'username' not in session:
         return redirect(url_for('login'))
     if request.method == 'POST':
-        
         database.user_feedback(session['username'], question, answer, int(request.form['score']),
                                request.form['expected_answer'])
         return redirect(url_for("welcome", username=session['username']))
